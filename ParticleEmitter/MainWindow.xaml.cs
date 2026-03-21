11aa8fc7b2303b75c4974495be95c4a4388063a6
JaykeBird <jacobrh94@hotmail.com>
2026-03-21 17:33:04 -0500

    Create ParticleEmiter test project

---
+++ ParticleEmitter/MainWindow.xaml.cs
@@ -0,0 +1,240 @@
+﻿using ParticleEmitter.PropEditor;
+using SolidShineUi;
+using SolidShineUi.KeyboardShortcuts;
+using System.Drawing.Drawing2D;
+using System.Runtime.ConstrainedExecution;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ParticleEmitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        KeyboardShortcutHandler ksh;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ksh = new KeyboardShortcutHandler(this);
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.F5,
+                new ActionKeyAction(Start, "Start", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.Ctrl, Key.F6,
+                new ActionKeyAction(Pause, "Pause", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.Delete,
+                new ActionKeyAction(Clear, "Clear", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.F8,
+                new ActionKeyAction(ShowHideSettings, "Settings", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.F9,
+                new ActionKeyAction(ChangeBackground, "Background", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.F10,
+                new ActionKeyAction(ShowHideTheme, "Theme", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.F11,
+                new ActionKeyAction(Fullscreen, "Fullscreen", this));
+            ksh.KeyRegistry.RegisterKeyShortcut(KeyboardCombination.None, Key.F12,
+                new ActionKeyAction(ShowHideUi, "ShowHideUi", this));
+
+            SsuiTheme = new SsuiAppTheme(SystemColors.AccentColorLight1);
+            selAccent.IsSelected = true;
+
+            pl.UnregisterEditor(typeof(uint));
+            pl.RegisterEditor(typeof(uint), typeof(LongEditor));
+            pl.ShowInheritedProperties = false;
+
+            Loaded += MainWindow_Loaded;
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            pl.LoadObject(emitter);
+            pl.ShowInheritedProperties = false;
+        }
+
+        private void MainWindow_Closing(object? sender, System.ComponentModel.CancelEventArgs e)
+        {
+            emitter.Shutdown();
+        }
+
+        bool hasStarted = false;
+
+        void Start()
+        {
+            if (!hasStarted)
+            {
+                emitter.Start();
+                hasStarted = true;
+            }
+            else
+            {
+                emitter.Resume();
+            }
+        }
+
+        void Pause()
+        {
+            emitter.Pause();
+        }
+
+        void Clear()
+        {
+            emitter.Clear();
+        }
+
+        void ShowHideSettings()
+        {
+            if (brdrSettings.Visibility == Visibility.Visible)
+            {
+                brdrSettings.Visibility = Visibility.Collapsed;
+                btnSettings.IsSelected = false;
+            }
+            else
+            {
+                brdrSettings.Visibility = Visibility.Visible;
+                pl.ReloadObject();
+                pl.ShowInheritedProperties = false;
+                btnSettings.IsSelected = true;
+            }
+        }
+
+        void ShowHideTheme()
+        {
+            if (selTheme.Visibility == Visibility.Visible)
+            {
+                selTheme.Visibility = Visibility.Collapsed;
+                btnTheme.IsSelected = false;
+            }
+            else
+            {
+                selTheme.Visibility = Visibility.Visible;
+                btnTheme.IsSelected = true;
+            }
+        }
+
+        void ChangeBackground()
+        {
+            Color windowCol = Colors.White;
+            if (Background is SolidColorBrush scb)
+            {
+                windowCol = scb.Color;
+            }
+            ColorPickerDialog cpd = new ColorPickerDialog(windowCol)
+            {
+                SsuiTheme = SsuiTheme,
+                Owner = this
+            };
+
+            cpd.ShowDialog();
+            if (cpd.DialogResult)
+            {
+                Background = new SolidColorBrush(cpd.SelectedColor);
+            }
+        }
+
+        void Fullscreen()
+        {
+            if (WindowState == WindowState.Maximized && WindowStyle == WindowStyle.None)
+            {
+                WindowStyle = WindowStyle.SingleBorderWindow;
+                WindowState = WindowState.Normal;
+            }
+            else
+            {
+                WindowStyle = WindowStyle.None;
+                WindowState = WindowState.Maximized;
+            }
+        }
+
+        void ShowHideUi()
+        {
+            if (toolbar.Visibility == Visibility.Visible)
+            {
+                toolbar.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                toolbar.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Start();
+        }
+
+        private void btnPause_Click(object sender, RoutedEventArgs e)
+        {
+            Pause();
+        }
+
+        private void btnClear_Click(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private void btnSettings_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideSettings();
+        }
+
+        private void btnBackground_Click(object sender, RoutedEventArgs e)
+        {
+            ChangeBackground();
+        }
+
+        private void btnTheme_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideTheme();
+        }
+
+        private void btnFullScreen_Click(object sender, RoutedEventArgs e)
+        {
+            Fullscreen();
+        }
+
+        private void btnShowHideUi_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideUi();
+        }
+
+        private void selTheme_SelectionChanged(object sender, RoutedSelectionChangedEventArgs<IClickSelectableControl> e)
+        {
+            IClickSelectableControl? ics = selTheme.Items.SelectedItems.FirstOrDefault();
+
+            if (ics != null)
+            {
+                switch (ics.Name)
+                {
+                    case nameof(selAccent):
+                        SsuiTheme = new SsuiAppTheme(SystemColors.AccentColorLight1);
+                        break;
+                    case nameof(selDark):
+                        SsuiTheme = SsuiThemes.CreateDarkTheme(SystemColors.AccentColorLight1);
+                        break;
+                    case nameof(selLight):
+                        SsuiTheme = SsuiThemes.CreateLightTheme(SystemColors.AccentColorLight1);
+                        break;
+                    case nameof(selHc1):
+                        SsuiTheme = SsuiThemes.HighContrastWhiteOnBlack;
+                        break;
+                    case nameof(selHc2):
+                        SsuiTheme = SsuiThemes.HighContrastGreenOnBlack;
+                        break;
+                    case nameof(selHc3):
+                        SsuiTheme = SsuiThemes.HighContrastBlackOnWhite;
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ ParticleEmitter/PropEditor/LongEditor.xaml.cs
@@ -0,0 +1,311 @@
+﻿using SolidShineUi.Utils;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using SolidShineUi.PropertyList;
+using SolidShineUi;
+
+namespace ParticleEmitter.PropEditor
+{
+    /// <summary>
+    /// A property editor for <see cref="long"/>, <see cref="uint"/>, and <see cref="ulong"/> types.
+    /// </summary>
+    public partial class LongEditor : UserControl, IPropertyEditor
+    {
+        /// <summary>
+        /// Create an IntegerEditor.
+        /// </summary>
+        public LongEditor()
+        {
+            InitializeComponent();
+
+            // load in string values
+            mnuCopy.Header = Strings.CopyFullValue;
+            mnuDisplayHex.Header = Strings.DisplayAsHex;
+            mnuSetNull.Header = Strings.SetAsNull;
+
+            btnEnableEdit.Content = Strings.DecreaseAndEdit;
+        }
+
+        /// <inheritdoc/>
+        public List<Type> ValidTypes => (new[] { typeof(long), typeof(uint), typeof(ulong), 
+            typeof(long?), typeof(uint?), typeof(ulong?) }).ToList();
+
+        /// <inheritdoc/>
+        public bool EditorAllowsModifying => true;
+
+        /// <inheritdoc/>
+        public void SetHostControl(IPropertyEditorHost host) { /* _host = host; */ }
+
+        /// <inheritdoc/>
+        public void ApplySsuiTheme(SsuiTheme theme)
+        {
+            intSpinner.SsuiTheme = theme;
+            btnMenu.SsuiTheme = theme;
+            imgMenu.Source = IconLoader.LoadIcon("ThreeDots", theme.IconVariation);
+            btnEnableEdit.SsuiTheme = theme;
+        }
+
+        /// <inheritdoc/>
+        public FrameworkElement GetFrameworkElement()
+        {
+            return this;
+        }
+
+        /// <inheritdoc/>
+        public bool IsPropertyWritable
+        {
+            get => btnMenu.IsEnabled;
+            set { intSpinner.IsEnabled = value; btnMenu.IsEnabled = value; }
+        }
+
+        Type _propType = typeof(long);
+
+        bool ulong2long = false;
+        ulong ulongbig = 0L;
+
+        bool _internalAction = false;
+
+#if NETCOREAPP
+        
+        /// <inheritdoc/>
+        public event EventHandler? ValueChanged;
+        
+        /// <inheritdoc/>
+        public object? GetValue()
+#else
+
+        /// <inheritdoc/>
+        public event EventHandler ValueChanged;
+        
+        /// <inheritdoc/>
+        public object GetValue()
+#endif
+        {
+            if (_propType == typeof(long))
+            {
+                return intSpinner.Value;
+            }
+            else if (_propType == typeof(uint))
+            {
+                return (uint)intSpinner.Value;
+            }
+            else if (_propType == typeof(ulong))
+            {
+                if (ulong2long)
+                {
+                    return ulongbig;
+                }
+                else
+                {
+                    return (ulong)intSpinner.Value;
+                }
+            }
+            else if (_propType == typeof(long?))
+            {
+                if (mnuSetNull.IsChecked)
+                {
+                    return null;
+                }
+                else
+                {
+                    return intSpinner.Value;
+                }
+            }
+            else if (_propType == typeof(uint?))
+            {
+                if (mnuSetNull.IsChecked)
+                {
+                    return null;
+                }
+                else
+                {
+                    return (uint)intSpinner.Value;
+                }
+            }
+            else if (_propType == typeof(ulong?))
+            {
+                if (mnuSetNull.IsChecked)
+                {
+                    return null;
+                }
+                else
+                {
+                    if (ulong2long)
+                    {
+                        return ulongbig;
+                    }
+                    else
+                    {
+                        return (ulong)intSpinner.Value;
+                    }
+                }
+            }
+            else
+            {
+                return intSpinner.Value;
+            }
+        }
+
+#if NETCOREAPP
+        /// <inheritdoc/>
+        public void LoadValue(object? value, Type type)
+#else
+        /// <inheritdoc/>
+        public void LoadValue(object value, Type type)
+#endif
+        {
+            _propType = type;
+
+            if (value == null)
+            {
+                SetAsNull();
+            }
+
+            LoadUi();
+
+            _internalAction = true;
+            if (value is ulong ul && ul > long.MaxValue)
+            {
+                // the value is larger than can be displayed as a long; thus, I'll just display the text and offer a way to edit from there
+                ulong2long = true;
+                ulongbig = ul;
+                intSpinner.Value = 0L;
+
+                txtUlongLong.Text = ul.ToString(NumberFormatInfo.CurrentInfo);
+                txtUlongLong.ToolTip = ul.ToString("D", NumberFormatInfo.CurrentInfo);
+
+                btnEnableEdit.Visibility = Visibility.Visible;
+                txtUlongLong.Visibility = Visibility.Visible;
+                mnuDisplayHex.IsEnabled = false;
+                intSpinner.Visibility = Visibility.Collapsed;
+            }
+            else if (value is uint ui)
+            {
+                intSpinner.Value = (long)ui;
+            }
+            else
+            {
+                intSpinner.Value = (long)(value ?? 0);
+            }
+            _internalAction = false;
+        }
+
+        void LoadUi()
+        {
+            if (_propType == typeof(long))
+            {
+                SetMinMax(long.MinValue, long.MaxValue);
+            }
+            else if (_propType == typeof(uint))
+            {
+                SetMinMax(uint.MinValue, uint.MaxValue);
+            }
+            else if (_propType == typeof(ulong))
+            {
+                SetMinMax(0, long.MaxValue);
+            }
+            else if (_propType == typeof(long?))
+            {
+                SetMinMax(long.MinValue, long.MaxValue);
+                mnuSetNull.IsEnabled = true;
+            }
+            else if (_propType == typeof(uint?))
+            {
+                SetMinMax(uint.MinValue, uint.MaxValue);
+                mnuSetNull.IsEnabled = true;
+            }
+            else if (_propType == typeof(ulong?))
+            {
+                SetMinMax(0, long.MaxValue);
+                mnuSetNull.IsEnabled = true;
+            }
+
+            void SetMinMax(long min, long max)
+            {
+                intSpinner.MaxValue = max;
+                intSpinner.MinValue = min;
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("CodeQuality", "IDE0051:Remove unused private members", Justification = "<Pending>")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0060:Remove unused parameter", Justification = "<Pending>")]
+        private void intSpinner_ValueChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (_internalAction) return;
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        void SetAsNull()
+        {
+            mnuSetNull.IsEnabled = true;
+            mnuSetNull.IsChecked = true;
+            intSpinner.IsEnabled = false;
+        }
+
+        void UnsetAsNull()
+        {
+            mnuSetNull.IsChecked = false;
+            intSpinner.IsEnabled = true;
+        }
+
+        private void mnuSetNull_Click(object sender, RoutedEventArgs e)
+        {
+            if (mnuSetNull.IsChecked)
+            {
+                UnsetAsNull();
+            }
+            else
+            {
+                SetAsNull();
+            }
+            ValueChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void mnuDisplayHex_Click(object sender, RoutedEventArgs e)
+        {
+            if (intSpinner.DisplayAsHex)
+            {
+                intSpinner.DisplayAsHex = false;
+                mnuDisplayHex.IsChecked = false;
+            }
+            else
+            {
+                intSpinner.DisplayAsHex = true;
+                mnuDisplayHex.IsChecked = true;
+            }
+        }
+
+        private void btnEnableEdit_Click(object sender, RoutedEventArgs e)
+        {
+            ulong2long = false;
+            btnEnableEdit.Visibility = Visibility.Collapsed;
+            txtUlongLong.Visibility = Visibility.Collapsed;
+            mnuDisplayHex.IsEnabled = true;
+            intSpinner.Visibility = Visibility.Visible;
+            intSpinner.Value = long.MaxValue;
+        }
+
+        private void mnuCopy_Click(object sender, RoutedEventArgs e)
+        {
+            if (ulong2long)
+            {
+                Clipboard.SetText(ulongbig.ToString("D", NumberFormatInfo.CurrentInfo));
+            }
+            else
+            {
+                if (intSpinner.DisplayAsHex)
+                {
+                    Clipboard.SetText(intSpinner.Value.ToString("X", NumberFormatInfo.CurrentInfo));
+                }
+                else
+                {
+                    Clipboard.SetText(intSpinner.Value.ToString("D", NumberFormatInfo.CurrentInfo));
+                }
+            }
+        }
+    }
+}
